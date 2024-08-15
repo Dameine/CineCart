c84e29bc6b4c62b12a7fcde7f1562e0d6061ddde
dameine yang <dameiney123@gmail.com>
2024-08-14 21:30:14 -0500

    favorite page worksgit add .

---
+++ client/src/Components/MovieCard.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import styles from "./MovieCard.module.css";
+import AuthService from "../utils/auth.js";
+
+const MovieCard = ({ movie, onClick, showDelete, onDelete }) => {
+   return (
+      <li
+         className={styles.cinemaItem}
+         style={{
+            backgroundImage: `url(${movie.Poster !== "N/A" ? movie.Poster : "default-image.jpg"})`,
+         }}
+      >
+         <p className={styles.cinemaPlot}>{movie.Plot}</p>
+         <p className={styles.cinemaPlot}>{movie.Title}</p>
+         <p className={styles.cinemaPlot}>{movie.Director}</p>
+         <p className={styles.cinemaPlot}>{movie.Genre}</p>
+         <p className={styles.cinemaPlot}>{movie.imdbRating}</p>
+         <p className={styles.cinemaPlot}>{movie.imdbID}</p>
+         {AuthService.loggedIn() && onClick && (
+            <button type="button" value={movie.imdbID} onClick={onClick}>
+               Add to favorite movies
+            </button>
+         )}
+         {showDelete && (
+            <button type="button" value={movie.imdbID} onClick={onDelete}>
+               Remove from favorite movies
+            </button>
+         )}
+      </li>
+   );
+};
+
+export default MovieCard;
