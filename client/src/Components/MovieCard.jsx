71d5550efa5b4332e5d9040b9fd9767c38fff828
Solia26 <161400353+Solia26@users.noreply.github.com>
2024-08-16 23:32:10 -0500

    style

--- client/src/Components/MovieCard.jsx
+++ client/src/Components/MovieCard.jsx
@@ -3,30 +3,41 @@ import styles from "./MovieCard.module.css";
 import AuthService from "../utils/auth.js";
 
 const MovieCard = ({ movie, onClick, showDelete, onDelete }) => {
-   return (
-      <li className="s.MovieCardContainer">
+  return (
+   <li>
+    <div className={styles.MovieCardContainer}>
       <img
-      src={movie.Poster !== "N/A" ? movie.Poster : "default-image.jpg"}
-      alt="Movie Poster"/>
-      
-         <p className={styles.cinemaPlot}>{movie.Plot}</p>
-         <p className={styles.cinemaPlot}>{movie.Title}</p>
-         <p className={styles.cinemaPlot}>{movie.Director}</p>
-         <p className={styles.cinemaPlot}>{movie.Genre}</p>
-         <p className={styles.cinemaPlot}>{movie.imdbRating}</p>
-         {/* <p className={styles.cinemaPlot}>{movie.imdbID}</p> */}
-         {AuthService.loggedIn() && onClick && (
-            <button type="button" value={movie.imdbID} onClick={onClick}>
-               Add to favorite movies
-            </button>
-         )}
-         {showDelete && (
-            <button type="button" value={movie.imdbID} onClick={onDelete}>
-               Remove from favorite movies
-            </button>
-         )}
-      </li>
-   );
+        src={movie.Poster !== "N/A" ? movie.Poster : "default-image.jpg"}
+        alt="Movie Poster"
+        id="movie-img"
+      />
+
+      <div className={styles.details}>
+        <p className={styles.cinemaPlot}>{movie.Plot}</p>
+        <p className={styles.cinemaPlot}>{movie.Title}</p>
+        <p className={styles.cinemaPlot}>{movie.Director}</p>
+        <p className={styles.cinemaPlot}>{movie.Genre}</p>
+        <p className={styles.cinemaPlot}>{movie.imdbRating}</p>
+        {/* <p className={styles.cinemaPlot}>{movie.imdbID}</p> */}
+        
+      </div>
+    </div>
+
+    {AuthService.loggedIn() && onClick && (
+          <button className={styles.movieBtn} type="button" value={movie.imdbID} onClick={onClick}>
+            Add to favorite movies
+          </button>
+        )}
+        {showDelete && (
+          <button className={styles.movieBtn} type="button" value={movie.imdbID} onClick={onDelete}>
+            Remove from favorite movies
+          </button>
+        )}
+
+
+   </li>
+   
+  );
 };
 
 export default MovieCard;
